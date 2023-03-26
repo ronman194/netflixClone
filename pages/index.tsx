72a1dddedfe53f9a823037ac80956883a84c13b8
ronman194 <mansharofron@gmail.com>
2023-03-26 13:13:11 +0300

    add movie card and display movies in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { signOut, getSession } from 'next-auth/react';
 
@@ -22,10 +24,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user} = useCurrentUser();
+  const { data: movies = []} = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   )
 }
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface MovieInterface {
+    id: string;
+    title: string;
+    description: string;
+    thumanailUrl: string;
+    videoUrl: string;
+    duration: string;
+    genre: string;
+}
